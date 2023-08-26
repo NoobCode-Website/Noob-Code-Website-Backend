11e34acfa4e7ce8c1b72824fab7ca972024e3279
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-08-27 02:31:13 +0530

    added a get request

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 
-const {addEventDetails, addSpeakerDetails}= require ("../controllers/eventController");
+const {addEventDetails, addSpeakerDetails, getEvents}= require ("../controllers/eventController");
 
 const eventrouter = express.Router();
 
 eventrouter.post ('/addevent', addEventDetails)
 eventrouter.post('/addspeaker', addSpeakerDetails)
+eventrouter.get ('/getevents', getEvents)
 
 module.exports= eventrouter;
